c1cc3bcfeb0f76bb8a13b066b5c5ea719c230090
destro0150 <realmunich61@gmail.com>
2025-09-30 22:39:52 -0300

    Update logger.lua

--- logger.lua
+++ logger.lua
@@ -2,6 +2,7 @@ function script.update(dt)
     local car = ac.getCar(0)
     if not car then return end
 
-    -- Loguea qué tipo de dato es
-    ac.debug("car.damage: " .. tostring(car.damage))
+    for k, v in pairs(car) do
+        ac.debug(tostring(k) .. " = " .. tostring(v))
+    end
 end
